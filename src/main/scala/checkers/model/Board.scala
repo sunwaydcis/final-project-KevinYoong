503381c75616944fe0a6fb14a45c37c55a092643
Yoong Kok Keong <kevinyoongkokkeong@gmail.com>
2025-01-10 17:33:13 +0800

    Update Board.scala
    Added comments for further clarification

--- src/main/scala/checkers/model/Board.scala
+++ src/main/scala/checkers/model/Board.scala
@@ -7,6 +7,7 @@ import scala.collection.mutable
 class Board {
   private val board: mutable.Map[(Int, Int), Piece] = mutable.Map()
 
+  // Initialize pieces on the board
   def initializePieces(selectedColor: String): Unit = {
     for (row <- 0 until 8; col <- 0 until 8 if (row + col) % 2 != 0) {
       if (MainApp.getSelectedColor().toLowerCase() == "white") {
@@ -25,19 +26,23 @@ class Board {
     }
   }
 
+  // Get the piece at a specific position
   def getPiece(row: Int, col: Int): Option[Piece] = {
     board.get((row, col))
   }
 
+  // Move a piece from one position to another
   def movePiece(startRow: Int, startCol: Int, endRow: Int, endCol: Int): Unit = {
     val piece = board.remove((startRow, startCol))
     piece.foreach(board((endRow, endCol)) = _)
   }
 
+  // Remove a piece from the board
   def removePiece(row: Int, col: Int): Unit = {
     board.remove((row, col))
   }
 
+  // Handle jump logic
   def handleJump(startRow: Int, startCol: Int, endRow: Int, endCol: Int): Unit = {
     val rowDiff = Math.abs(endRow - startRow)
     if (rowDiff == 2) {
